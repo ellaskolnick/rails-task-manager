7ff82c58121b6b60762ce92133c4504f0904b396
Ella Skolnick <ellaskolnick@me.com>
2022-02-15 14:45:09 +0000

    icons and refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,17 +18,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(strong_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -35,6 +33,10 @@ class TasksController < ApplicationController
   private
 
   def strong_params
-    params.require(:task).permit(:title, :details, :complete)
+    params.require(:task).permit(:title, :details, :checkbox_value, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
